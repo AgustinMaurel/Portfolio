af9bec285c58458a0c94b457c565d5c80df112cd
Agustin Maurel <maurel.a97@gmail.com>
2024-10-04 18:20:45 -0300

    change color bg and css Switcher

--- portfolio/src/Components/LangSwitcher/LangSwitcher.tsx
+++ portfolio/src/Components/LangSwitcher/LangSwitcher.tsx
@@ -1,27 +1,42 @@
 'use client'
 
-import { ChangeEvent } from "react";
+import { MouseEvent } from "react";
 import { GrLanguage } from "react-icons/gr";
 import { useState } from "react";
-
-
+import { useRouter } from "next/navigation";
 
 
 const LangSwitcher = () => {
 
     const [flag, setFlag]=useState<boolean>(false)
-    const onSelectChange= (e: ChangeEvent<HTMLSelectElement>)=>{
-        console.log(e.target.value)
+
+
+    const router = useRouter();
+
+    const onSelect = (event: MouseEvent<HTMLButtonElement>)=>{
+
+        const finalValue  = event.target as HTMLButtonElement
+        const nextLang = finalValue.value
+        router.replace(nextLang)
         setFlag(false)
+
         }
+
+    const handleClick = ()=>{
+        setFlag(true)
+    }
   return (
     <label className='flex h-full items-center justify-center'>
-      <GrLanguage onClick={()=>setFlag(true)} className="CustomButtonNavbar hover:cursor-pointer"/>
+        <button onClick={handleClick}>
+      <GrLanguage  className="CustomButtonNavbar hover:cursor-pointer"/>
+
+        </button>
         
-        {flag?<select   className={flag?'bg-ecru overflow-auto absolute mt-5 border rounded':'sr-only'}id="" onChange={onSelectChange}>
-            <option  value="es">Spanish</option>
-            <option value="en">English</option>
-        </select>: ""}
+        {flag?
+        <div onMouseLeave={()=>{setFlag(false)}} className={flag?'flex flex-col items-center text-sm justify-center border-licor w-[70px] gap-2 border-2 rounded-tl rounded-b bg-ecru text-licor p-1 absolute mt-20 mr-10 ':'sr-only'}id="">
+            <button className="CustomButtonNavbar hover:font-bold hover:text-licor" onClick={onSelect}  value="es">Spanish</button>
+            <button className="CustomButtonNavbar hover:font-bold hover:text-licor" onClick={onSelect} value="en">English</button>
+        </div>: ""}
     </label>
   )
 }
--- portfolio/src/app/[locale]/page.tsx
+++ portfolio/src/app/[locale]/page.tsx
@@ -6,7 +6,7 @@ import { unstable_setRequestLocale } from "next-intl/server";
 export default function Home({params:{locale}}:Readonly<{params: {locale:string}}>) {
   unstable_setRequestLocale(locale);
   return (
-    <div className="flex  bg-gradient-to-l from-licor to-cream/80 marker:w-full  items-center justify-center min-h-screen font-[family-name:var(--font-geist-sans)]">
+    <div className="flex  bg-gradient-to-l from-licor to-black/80 marker:w-full  items-center justify-center min-h-screen font-[family-name:var(--font-geist-sans)]">
       <div id='PageDiv' className="flex flex-col h-full w-full max-w-screen-lg">
         <About/>
         <Skills/>
