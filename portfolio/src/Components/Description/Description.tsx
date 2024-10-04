33747193a9f54a9856632ec10adcc05fa6e1dbf3
Agustin Maurel <maurel.a97@gmail.com>
2024-10-04 12:54:53 -0300

    add locate

--- portfolio/src/Components/Description/Description.tsx
+++ portfolio/src/Components/Description/Description.tsx
@@ -1,14 +1,16 @@
+import { useTranslations } from "next-intl"
+
 export default function Description(){
 
-
-            
+    const t = useTranslations()
+ 
     const text:string ='quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas Letraset, las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum. r ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum. r ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum. r ejemplo Aldus PageMaker, el cual incluye versiones de LorageMaker, el cualorem Ipsum. '
     return (
         <div id="description" className=" flex  text-white lg:pb-56 h-full max-h-screen min-h-screen w-full justify-start items-center ">
 
 
             <div className="flex flex-col w-full h-full justify-center items-start gap-2">
-                <h1 className="font-bold text-xl pb-2">Sobre Mi</h1>
+                <h1 className="font-bold text-xl pb-2">{t("Index.title")}</h1>
                 <p  className="  text-wrap w-full bg-inherit  outline-none ">
                     {text}
                 </p>
--- portfolio/src/app/[locale]/page.tsx
+++ portfolio/src/app/[locale]/page.tsx
@@ -1,8 +1,10 @@
 import Skills from "@/Components/Skills/Skills";
 import About from "@/Components/About/About";
 import Recommendations from "@/Components/Recommendations/Recommendations";
+import { unstable_setRequestLocale } from "next-intl/server";
 
-export default function Home() {
+export default function Home({params:{locale}}:Readonly<{params: {locale:string}}>) {
+  unstable_setRequestLocale(locale);
   return (
     <div className="flex  bg-gradient-to-l from-licor to-cream/80 marker:w-full  items-center justify-center min-h-screen font-[family-name:var(--font-geist-sans)]">
       <div id='PageDiv' className="flex flex-col h-full w-full max-w-screen-lg">
