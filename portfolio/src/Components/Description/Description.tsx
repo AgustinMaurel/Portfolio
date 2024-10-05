835637d6ad368e2e39910a9544616a56f4ee850d
Agustin Maurel <maurel.a97@gmail.com>
2024-10-05 17:15:21 -0300

    Css changes

--- portfolio/src/Components/Description/Description.tsx
+++ portfolio/src/Components/Description/Description.tsx
@@ -11,7 +11,7 @@ export default function Description(){
             <div className="flex flex-col  w-full h-full justify-center items-start gap-1">
                 <span className="font-bold text-2xl">{t("About.pretitle")}</span>
                 <h1 className="font-bold text-[50px] pb-2">{t("About.title")}</h1>
-            <p  className=" text-xl text-wrap w-full bg-inherit  outline-none ">
+            <p  className=" text-lg text-wrap w-full bg-inherit  outline-none ">
                     {t("About.description")}
                 </p>
             </div>
