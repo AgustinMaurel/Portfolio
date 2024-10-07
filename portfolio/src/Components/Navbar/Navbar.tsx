800de4eac774b8e9670899b50e3c492518008c12
Agustin Maurel <maurel.a97@gmail.com>
2024-10-07 10:08:58 -0300

    Change Navbar location

--- portfolio/src/Components/Navbar/Navbar.tsx
+++ portfolio/src/Components/Navbar/Navbar.tsx
@@ -1,45 +1,68 @@
 'use client'
 
-import { FaHome } from "react-icons/fa";
-import { useEffect, useState } from "react";
+
+import { MouseEvent, useEffect, useState } from "react";
 import CustomButtonNavbar from "../CustomButtonNavbar/CustomButtonNavbar";
-import Link from "next/link";
+
 import LangSwitcher from "../LangSwitcher/LangSwitcher";
 import { useTranslations } from "next-intl";
 
 
+
+
 export default function Navbar(){
+    
 
     const t =useTranslations()
-    
+
+    const [section, setSection] = useState<string>("#About")
+
+    const handleSection = (e : MouseEvent<HTMLAnchorElement> ) =>{
+        setSection(e.target.hash)
+    }
+
+    const activeClass="border-2 w-2/12 transition-all motion-reduce:transition-none ease-in-out duration-300"
+    const offClass="border-2 w-1/12  transition-all motion-reduce:transition-none"
+
     const [isClient, setIsClient]= useState<boolean>(false)
     useEffect(()=>{
-        setIsClient(true)
+        setIsClient(true)    
     },[])
+
     return (
-        <nav className="flex fixed top-0 z-50 justify-center items-center text-lg font-semibold  h-14 bg-transparent w-full border-none">
+        <nav className="cursor-all-scroll flex flex-col fixed w-full  top-0 bottom-0 items-center justify-center  max-w-screen-md">
                 
             {isClient?<div className="flex justify-between items-center h-full w-full max-w-screen-2xl ">
 
-                    <div  className=" flex h-full justify-center items-center gap-2 CustomButtonNavbar hover:cursor-pointer" >
+                    {/* <div  className=" flex h-full justify-center items-center gap-2 CustomButtonNavbar hover:cursor-pointer" >
                     <Link href={"/#About"}>
                     <FaHome className="CustomButtonNavbar hover:cursor-pointer w-6 h-6"  href=""/>
                     </Link>
 
                     <CustomButtonNavbar title={"Inicio"} href="/#About" styles="CustomButtonNavbar  w-full"/>
 
-                    </div>
+                    </div> */}
                 
 
-                <ul className="flex  items-center justify-end gap-4 w-full ">
-                    <CustomButtonNavbar title={t("Navbar.about")} href="#About" styles="CustomButtonNavbar"/>
-                    <CustomButtonNavbar title={t("Navbar.skills")} href="#Skills" styles="CustomButtonNavbar"/>
-                    <CustomButtonNavbar title={t("Navbar.projects")} href="#Recommendations" styles="CustomButtonNavbar"/>
-                    
+                <div className="flex flex-col  items-start justify-end gap-4 w-full ">
+                        <div className={section.includes("About")? "flex w-full justify-start gap-2 items-center":"flex w-full opacity-40 justify-start gap-2 items-center"}>
+                            <div className={section.includes("About") ? activeClass : offClass}/>
+                             <CustomButtonNavbar handleClick={handleSection}  title={t("Navbar.about")} href="#About" styles="CustomButtonNavbar hover:font-semibold"/>
+                        </div>
+
+                        <div className={section.includes("Skills")? "flex w-full justify-start gap-2 items-center":"flex w-full opacity-40 justify-start gap-2 items-center"}>
+                            <div className={section.includes("Skills") ? activeClass :offClass}/>
+                            <CustomButtonNavbar handleClick={handleSection} title={t("Navbar.skills")} href="#Skills" styles="CustomButtonNavbar hover:font-semibold"/>
+                        </div>
+                        <div className={section.includes("Recommendations")? "flex w-full justify-start gap-2 items-center":"flex w-full opacity-40 justify-start gap-2 items-center"}>
+                            <div className={section.includes("Recommendations") ? activeClass : offClass}/>
+                             <CustomButtonNavbar handleClick={handleSection} title={t("Navbar.projects")} href="#Recommendations" styles="CustomButtonNavbar hover:font-semibold"/>
+                        </div>
+                       
                    <LangSwitcher/>
                     
                     
-                </ul>
+                </div>
             
             </div>: ''}
         </nav>
