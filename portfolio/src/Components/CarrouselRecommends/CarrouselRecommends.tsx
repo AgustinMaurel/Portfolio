3f59deb4fc524ee1b7a7f96625986f3022d98643
Agustin Maurel <maurel.a97@gmail.com>
2024-10-07 13:36:08 -0300

    add carrousel and recomend card comps

--- portfolio/src/Components/CarrouselRecommends/CarrouselRecommends.tsx
+++ portfolio/src/Components/CarrouselRecommends/CarrouselRecommends.tsx
@@ -1,11 +1,76 @@
-import React from 'react'
+'use client'
 
-const CarrouselRecommends = () => {
+import { EmblaOptionsType } from 'embla-carousel'
+
+import useEmblaCarousel from "embla-carousel-react"
+import { CardRecommend } from "@/types"
+import CustomCardRecommend from "../CustomCardRecommend/CustomCardRecommend"
+import Autoplay from "embla-carousel-autoplay"
+
+type CarrouselProps = {
+  slidesOnScreen :number,
+  options?: EmblaOptionsType,
+  
+}
+
+const allRefs : CardRecommend[] =[
+  {
+    description:"He was a valuable team member, possessing expertise and a proactive approach to problem-solving. His teamwork and effective communication with the UX team helped reach our goals.",
+    img: "/img1.jpg",
+    subtitle: "Diseñador de Producto, Gráfico & Multimedial | Tutor",
+    title: " Fernando Gabriel Villabrille",
+    key:1
+  },
+  {
+    description:"He was a valuable team member, possessing expertise and a proactive approach to problem-solving. His teamwork and effective communication with the UX team helped reach our goals.",
+    img: "/img2.jpg",
+    subtitle: "Diseñador de Producto, Gráfico & Multimedial | Tutor",
+    title: " Fernando Gabriel Villabrille",
+    key:2
+  },
+  {
+    description:"He was a valuable team member, possessing expertise and a proactive approach to problem-solving. His teamwork and effective communication with the UX team helped reach our goals.",
+    img: "/img3.jpg",
+    subtitle: "Diseñador de Producto, Gráfico & Multimedial | Tutor",
+    title: " Fernando Gabriel Villabrille",
+    key:3
+  },
+  
+]
+export default function CarrouselRecommends ({ slidesOnScreen=1, options}: CarrouselProps) {
+
+  const [emblaRef, emblaApi]= useEmblaCarousel({
+    ...options,
+    align: 'start',
+    slidesToScroll: slidesOnScreen,
+    containScroll: 'trimSnaps',
+  },
+  [Autoplay()],
+)
+
+  
+
+
+
   return (
-    <div>
-      
+    <div className="flex flex-col w-10/12 h-full gap-2 items-center rounded-md  justify-center  overflow-hidden">
+      <h1 className='text-xl font-semibold'>Recommendations</h1>
+      <div className="w-full overflow-hidden" ref={emblaRef}>
+        <div className="flex min-h-fit w-full">
+          {allRefs.map((item) => (
+            <div
+              className={`flex w-full min-w-0 flex-none items-center justify-center ${slidesOnScreen > 1 ? 'mr-4 md:w-[calc(50%-8px)]' : 'mr-1'}`}
+              key={item.key}
+            >
+              <CustomCardRecommend description={item.description} key={item.key} img={item.img} title={item.title} subtitle={item.subtitle}/>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="flex w-full items-center justify-center gap-x-12">
+   
+      </div>
     </div>
   )
 }
 
-export default CarrouselRecommends
--- portfolio/src/Components/CustomCardRecommend/CustomCardRecommend.tsx
+++ portfolio/src/Components/CustomCardRecommend/CustomCardRecommend.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { CardRecommend } from '@/types'
+import Image from 'next/image'
 
-const CustomCardRecommend = () => {
+
+const CustomCardRecommend = ({description, key, img, subtitle,title}:CardRecommend) => {
   return (
-    <div>
+    <div key={key} className='w-full flex flex-col bg-wine/10 rounded-md px-4 py-2 justify-center items-center'>
+        <div className='w-full flex justify-start gap-3 items-center'>
+
+        <Image  width={80} height={80} className='rounded-full h-full' alt={title} src={img} />
+            <div>
+                <h1>{title}</h1>
+                <h2>{subtitle}</h2>
+
+            </div>
+        </div>
+
+        <p>{description}</p>
+        
       
     </div>
   )
