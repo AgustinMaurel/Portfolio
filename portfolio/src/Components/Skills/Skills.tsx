41fbfe57b71d46b7794d15c706b9777f44a776fb
Agustin Maurel <maurel.a97@gmail.com>
2024-10-08 15:17:01 -0300

    add comps skill/skillcard and messages

--- portfolio/src/Components/Skills/Skills.tsx
+++ portfolio/src/Components/Skills/Skills.tsx
@@ -1,9 +1,14 @@
+import { useTranslations } from "next-intl"
+
 export default function Skills () {
+
+        const t = useTranslations()
 
         return(
 
-            <div id="Skills" className="flex w-full h-full min-h-screen max-w-screen-md justify-center items-center border-ecru border-2">
-                     <span>Skills</span> 
+            <div id="Skills" className="flex flex-col w-full h-full min-h-screen max-w-screen-md justify-center items-start gap-10 ">
+                     <h1 className="text-2xl font-semibold">{t("Skills.title")}</h1>
+                     <p className="text-lg text-wrap">{t("Skills.intro")}</p>
              </div>
 
 
