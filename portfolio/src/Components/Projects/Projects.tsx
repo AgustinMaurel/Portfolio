20cf0f0371276a8d0199651957a566b940300152
Agustin Maurel <maurel.a97@gmail.com>
2024-10-08 17:12:45 -0300

    add projects section/messages

--- portfolio/src/Components/Projects/Projects.tsx
+++ portfolio/src/Components/Projects/Projects.tsx
@@ -1,12 +1,13 @@
+import { useTranslations } from "next-intl"
+
 export default function Projects(){
     
-
+const t = useTranslations("Projects")
         return(
 
-            <div id="Projects" className="flex w-full h-full justify-center items-center min-h-screen border-2 border-ecru">
-                    <span>Projects
-
-                    </span>
+            <div id="Projects" className="flex flex-col w-full h-full justify-center gap-10 items-start mb-20 pt-20 ">
+                    <h1 className="text-2xl font-semibold">{t("title")}</h1>
+                    <p className="text-lg">{t("intro")}</p>
                     
              </div>
 
