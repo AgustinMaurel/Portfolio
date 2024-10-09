f31bace710ada1bbada7acb9bbc155712edfbaa1
Agustin Maurel <maurel.a97@gmail.com>
2024-10-09 13:32:38 -0300

    fix skills css

--- portfolio/src/Components/Skills/Skills.tsx
+++ portfolio/src/Components/Skills/Skills.tsx
@@ -9,9 +9,9 @@ export default function Skills () {
         return(
 
             <div id="Skills" className="flex flex-col w-full max-h-screen max-w-screen-md justify-center pt-20 items-start gap-y-10 ">
+
                      <h1 className="text-2xl font-semibold">{t("title")}</h1>
                      <p className="text-lg w-11/12 text-wrap">{t("intro")}</p>
-
 
                 <div className="flex w-11/12 justify-start items-start gap-5"> 
                      <SkillsCard allSkills={techSkills} title="tech" titleKey="hard"/>   
--- portfolio/src/Components/SkillsCard/SkillsCard.tsx
+++ portfolio/src/Components/SkillsCard/SkillsCard.tsx
@@ -12,11 +12,11 @@ const SkillsCard = ({allSkills,title,titleKey}:skillCardProps) => {
 const t =useTranslations("Skills")
 
   return (
-    <div key={title} className=" h-full full gap-3 flex flex-wrap justify-center items-center px-3 py-4  bg-wine/10 rounded-lg ">
+    <div key={title} className=" h-full full gap-3 flex flex-wrap justify-start items-center px-3 py-4  bg-wine/10 rounded-lg ">
         
         <h2 className="text-lg">{t(titleKey)}</h2>
 
-        <div className=" h-full full gap-3 flex flex-wrap justify-center items-center ">
+        <div className=" h-full full gap-3 flex flex-wrap justify-start items-center ">
 
             {
             allSkills.map((e)=>{
