96ab968c401716c1e5b74b18d47e5423643debd5
Agustin Maurel <maurel.a97@gmail.com>
2024-10-13 16:02:46 -0300

    test

--- portfolio/src/Components/Wizard/Wizard.jsx
+++ portfolio/src/Components/Wizard/Wizard.jsx
@@ -1,27 +1,32 @@
-'use client'
-import { useGLTF } from '@react-three/drei'
+// 'use client'
+// import { useGLTF } from '@react-three/drei'
 
-export function Wizard() {
 
-  const { nodes, materials } = useGLTF('/wizard.glb')
-  const cube  = nodes.Cube002_0
-  const finalGeometry  = cube.geometry
-  return (
+// export function Wizard() {
+
+//     const { nodes, materials } = useGLTF('/wizard.glb')
+  
+//     const cube  = nodes.Cube002_0
+//     const finalGeometry  = cube.geometry
+
+ 
+
+//   return (
     
-    <group   dispose={null}>
-      <group rotation={[-Math.PI / 2, 0, 0]}>
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={finalGeometry} 
-          material={materials.color}
-          scale={[0.245, 0.245, 0.212]}
+//     <group   dispose={null}>
+//       <group rotation={[-Math.PI / 2, 0, 0]}>
+//         <mesh
+//           castShadow
+//           receiveShadow
+//           geometry={finalGeometry} 
+//           material={materials.color}
+//           scale={[0.245, 0.245, 0.212]}
           
           
-        />
-      </group>
-    </group>
-  )
-}
+//         />
+//       </group>
+//     </group>
+//   )
+// }
 
-useGLTF.preload('wizard.glb')
+// useGLTF.preload('wizard.glb')
