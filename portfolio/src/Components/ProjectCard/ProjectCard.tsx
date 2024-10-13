3dd8b9190b6fc7fd3cb19fef3ee543b874029cb6
Agustin Maurel <maurel.a97@gmail.com>
2024-10-13 16:26:00 -0300

    console.log img

--- portfolio/src/Components/ProjectCard/ProjectCard.tsx
+++ portfolio/src/Components/ProjectCard/ProjectCard.tsx
@@ -7,6 +7,7 @@ import { FaGithub } from 'react-icons/fa'
 
 const ProjectCard = ({title, img, alt, description, techs, url}:projectCardProps) => {
 
+  console.log(img)
 const t = useTranslations("Projects")
   
   return (
